8a9c2d5f50e788d2d624620a8a181fff82fdc0fd
Pablo Gonzalez <pablojava853@outlook.com>
2021-07-18 18:56:25 -0300

    Geração de classes com plugin xsd

---
+++ src/main/java/br/com/pablodelphos/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2021.07.18 às 06:42:22 PM BRT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://pablodelphos.com.br", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package br.com.pablodelphos;
