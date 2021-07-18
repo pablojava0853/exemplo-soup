34e4772b464b122bc335bdbf3d573cf44ad955c1
Pablo Gonzalez <pablojava853@outlook.com>
2021-07-18 17:22:59 -0300

    Primeiro commit

---
+++ src/main/java/com/soup/delphos/DelphosApplication.java
@@ -0,0 +1,13 @@
+package com.soup.delphos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelphosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelphosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soup/delphos/DelphosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soup.delphos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelphosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
